6f22ac2bba94813b5670572b719597994b450552
Weera Wu <weera@oozou.com>
2019-04-01 17:49:35 +0700

    Finish Creating Elements With CreateElement

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = React.createElement(
+  "section",
+  { className: "section" },
+  React.createElement(
+    "div",
+    { className: "container" },
+    React.createElement("h1", { className: "title" }, "Hello World")
+  )
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(element, document.getElementById("root"));
