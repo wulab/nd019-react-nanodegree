06d71cc7df8a11efcef0685155472a6957223e89
Weera Wu <weera@oozou.com>
2019-04-01 20:55:36 +0700

    Finish ReactCreateElement To JSX

--- src/index.js
+++ src/index.js
@@ -4,20 +4,16 @@ import "./index.css";
 
 const people = [{ name: "Tyler" }, { name: "Karen" }, { name: "Richard" }];
 
-const element = React.createElement(
-  "section",
-  { className: "section" },
-  React.createElement(
-    "div",
-    { className: "container" },
-    React.createElement(
-      "ol",
-      null,
-      people.map(person =>
-        React.createElement("li", { key: person.name }, person.name)
-      )
-    )
-  )
+const element = (
+  <section className="section">
+    <div className="container">
+      <ol>
+        {people.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    </div>
+  </section>
 );
 
 ReactDOM.render(element, document.getElementById("root"));
