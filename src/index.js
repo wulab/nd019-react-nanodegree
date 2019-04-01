4a7b1c287459318a7be33f9266f11c7fabf1970b
Weera Wu <weera@oozou.com>
2019-04-01 18:33:03 +0700

    Finish Nesting With An Array

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const people = [{ name: "Tyler" }, { name: "Karen" }, { name: "Richard" }];
+
 const element = React.createElement(
   "section",
   { className: "section" },
   React.createElement(
     "div",
     { className: "container" },
-    React.createElement("h1", { className: "title" }, "Hello World")
+    React.createElement(
+      "ol",
+      null,
+      people.map(person =>
+        React.createElement("li", { key: person.name }, person.name)
+      )
+    )
   )
 );
 
