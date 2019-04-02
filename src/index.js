01ed23ca05c4217535cf048c5efce01509c155df
Weera Wu <weera@oozou.com>
2019-04-02 09:14:40 +0700

    Finish Create A Component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const people = [{ name: "Tyler" }, { name: "Karen" }, { name: "Richard" }];
+class ContactList extends React.Component {
+  render() {
+    const people = [{ name: "Tyler" }, { name: "Karen" }, { name: "Richard" }];
 
-const element = (
-  <section className="section">
-    <div className="container">
-      <ol>
-        {people.map(person => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ol>
-    </div>
-  </section>
-);
+    return (
+      <section className="section">
+        <div className="container">
+          <ol>
+            {people.map(person => (
+              <li key={person.name}>{person.name}</li>
+            ))}
+          </ol>
+        </div>
+      </section>
+    );
+  }
+}
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<ContactList />, document.getElementById("root"));
