3ec21d9434493fc4dbaac6c735364a8e088f9cc5
Weera Wu <weera@oozou.com>
2019-04-04 18:20:08 +0700

    Finish Displaying The Contact Names

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class ListContacts extends Component {
   render() {
     return (
-      <ol className="contact-list"></ol>
+      <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ol>
     );
   }
 }
