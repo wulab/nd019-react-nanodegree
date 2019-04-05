573387b00bc449bfd365c018117a7fd269a3345f
Weera Wu <weera@oozou.com>
2019-04-05 23:27:42 +0700

    Finish Add The Now Showing Details

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -11,6 +11,8 @@ class ListContacts extends Component {
 
   updateQuery = query => this.setState({ query: query.trim() });
 
+  clearQuery = () => this.updateQuery('');
+
   render() {
     const { query } = this.state;
     const { contacts, onRemoveContact } = this.props;
@@ -30,6 +32,14 @@ class ListContacts extends Component {
             onChange={event => this.updateQuery(event.target.value)}
           />
         </div>
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} of {contacts.length} contacts
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
         <ol className="contact-list">
           {showingContacts.map(contact => (
             <li key={contact.id} className="contact-list-item">
