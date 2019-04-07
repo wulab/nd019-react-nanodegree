53c8adb572a7e81a635de2528a78d6367f2c5242
Weera Wu <weera@oozou.com>
2019-04-07 17:46:00 +0700

    Finish Build Out The Contact Form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   render() {
-    return <div>Create Contact</div>;
+    return (
+      <div>
+        <Link to="/" className="close-create-contact">
+          Close
+        </Link>
+        <form className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name" />
+            <input type="text" name="handle" placeholder="Handle" />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
