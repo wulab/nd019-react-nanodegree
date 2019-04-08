6dfc4d2de22eb759cf75e985df2ba7c6e692c154
Weera Wu <weera@oozou.com>
2019-04-08 17:57:37 +0700

    Finish Handle Submission Of The Contact Form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serialize from 'form-serialize';
 
 class CreateContact extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+    const contact = serialize(event.target, { hash: true });
+
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(contact);
+    }
+  };
+
   render() {
     return (
       <div>
         <Link to="/" className="close-create-contact">
           Close
         </Link>
-        <form className="create-contact-form">
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name="avatarURL"
