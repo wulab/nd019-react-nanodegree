f5ff67bb248eadad70d53d505a6be5e4be6e2cda
Weera Wu <weera@oozou.com>
2019-04-20 18:07:32 +0700

    Finish Listening To Changes

--- src/reactnd-redux-todos-goals/index.js
+++ src/reactnd-redux-todos-goals/index.js
@@ -1,9 +1,20 @@
 function createStore() {
   let state;
+  let listeners = [];
 
   const getState = () => state;
 
+  const subscribe = listener => {
+    listeners.push(listener);
+  };
+
   return {
-    getState
+    getState,
+    subscribe
   };
 }
+
+const store = createStore();
+
+store.subscribe(() => console.log('The store changed.'));
+store.subscribe(() => console.log(`The new state is: ${store.getState()}`));
