20c9feb4ed16429f4d93b7c5608f7b72a9a6e06b
Weera Wu <weera@oozou.com>
2019-04-24 18:40:22 +0700

    Finish Identifying The Library Code

--- src/reactnd-redux-todos-goals/index.js
+++ src/reactnd-redux-todos-goals/index.js
@@ -1,4 +1,10 @@
-function createStore() {
+// Library code
+function createStore(reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
   let state;
   let listeners = [];
 
@@ -13,8 +19,23 @@ function createStore() {
     listeners = listeners.filter(l => l !== listener);
   };
 
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   };
+}
+
+// App code
+function todos(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo]);
+  }
+
+  return state;
 }
