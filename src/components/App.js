09647c962c7b7657aff8fb8326cd5de4807fbef8
Weera Wu <weera@oozou.com>
2019-06-23 15:15:08 +0700

    Finish L704 Starter Code V2

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3 className="center">Starter Code</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
